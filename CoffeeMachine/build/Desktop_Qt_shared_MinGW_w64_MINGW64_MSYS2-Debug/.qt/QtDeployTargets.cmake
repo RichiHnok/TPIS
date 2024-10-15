3636af0b93337b9df1b72c4d0d96d8f49ecd673e
Artemiy Viazovski <grenkavoit@gmail.com>
2024-10-15 14:09:50 +0300

    Revert "somecom"
    This reverts commit 0e8e7f6e3dcc2bbdc35cfc8bcb21b15c783ba97f.

---
+++ CoffeeMachine/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_CoffeeMachine_FILE D:/RichiFiles/JavaProjects/MAIS/lab1/TPIS/CoffeeMachine/build/Desktop_Qt_shared_MinGW_w64_MINGW64_MSYS2-Debug/CoffeeMachine.exe)
+set(__QT_DEPLOY_TARGET_CoffeeMachine_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_CoffeeMachine_RUNTIME_DLLS C:/ForInstallation/msys64/mingw64/bin/Qt6Widgets.dll;C:/ForInstallation/msys64/mingw64/bin/Qt6Gui.dll;C:/ForInstallation/msys64/mingw64/bin/Qt6Core.dll)
